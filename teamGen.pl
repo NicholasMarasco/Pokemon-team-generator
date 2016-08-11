b89a227c431171813e968d47a08e286aeb4d95a4
Nicholas Marasco <nicdmarasco@gmail.com>
2016-08-11 10:47:31 -0400

    Added debug levels for more or less info. Command line arguments now support bundling

--- teamGen.pl
+++ teamGen.pl
@@ -5,11 +5,12 @@
 use strict;
 use File::Basename;
 use Getopt::Long;
+Getopt::Long::Configure ("bundling");
 
 # Usage variables
 my $cmd = basename($0);
-my $options = " options:\n".
-              "   -d, --debug   Displays debug information and warnings\n";
+my $options = "\nOptions:\n".
+              "   -d, --debug   Increase debug information and warning level\n";
 my $usage = "usage: $cmd [-d] <number-of-teams> <data-file>\n".$options;
 
 # Option variables
@@ -17,8 +18,8 @@ my $help = 0;
 my $debug = 0;
 
 my $result = GetOptions(
-  "help|usage" => \$help,  # print help and exit
-  "debug"      => \$debug, # print debug info
+  "h|help|usage" => \$help,  # print help and exit
+  "d|debug+"     => \$debug, # print debug info
   );
 
 # Check for formatting or help
@@ -38,5 +39,16 @@ print "datafile: $datafile\n" if $debug;
 die "No such file exists: $datafile\n" if(! -e $datafile);
 open(FILE, '<',$datafile) or die "Cannot open file for reading: $datafile";
 
-print "We good.\n";
+# Process file and get hash of number to name
+my %mons;
+while(<FILE>){
+  chomp;
+  next if /^\s*#/;
+  next if /^\s*$/;
+  my @line = split/:/;
+  $mons{$line[0]}=$line[1];
+  print "$line[0] => $line[1]\n" if $debug > 1;
+}
 close FILE;
+
+print "\nWe good.\n";
