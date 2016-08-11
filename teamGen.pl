63541b617a9395a46295864fc7eccbd7794108e5
Nicholas Marasco <nicdmarasco@gmail.com>
2016-08-11 09:56:57 -0400

    Added command line option for debug information

--- teamGen.pl
+++ teamGen.pl
@@ -1,23 +1,40 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 # Perl script to generate random pokemon team from a given list
 
 use strict;
 use File::Basename;
+use Getopt::Long;
 
 # Usage variables
 my $cmd = basename($0);
-my $usage = "usage: $cmd <number-of-teams> <data-file>\n";
+my $options = " options:\n".
+              "   -d, --debug   Displays debug information and warnings\n";
+my $usage = "usage: $cmd [-d] <number-of-teams> <data-file>\n".$options;
 
-# Check for formatting
-die "$usage\n" if(@ARGV != 2);
+# Option variables
+my $help = 0;
+my $debug = 0;
+
+my $result = GetOptions(
+  "help|usage" => \$help,  # print help and exit
+  "debug"      => \$debug, # print debug info
+  );
+
+# Check for formatting or help
+die "$usage\n" if(@ARGV != 2
+                  or !$result
+                  or $help
+                 );
 
 # Team size parameter sanity check
 my $teamNum = $ARGV[0];
-die "$usage\nInvalid number of teams: $teamNum\n" if $teamNum =~ /\D+/;
+print "teamNum: $teamNum\n" if $debug;
+die "Invalid number of teams: $teamNum\n" if $teamNum =~ /\D+/;
 
 # Check if file exists and is readable
 my $datafile = $ARGV[1];
+print "datafile: $datafile\n" if $debug;
 die "No such file exists: $datafile\n" if(! -e $datafile);
 open(FILE, '<',$datafile) or die "Cannot open file for reading: $datafile";
 
